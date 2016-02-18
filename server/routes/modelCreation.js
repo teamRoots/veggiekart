c4b90b398cb38d8834cf68aecceb90a13c5f99ca
zach <bmadzac@gmail.com>
2016-02-18 11:51:39 -0600

    possible model creation

--- server/routes/modelCreation.js
+++ server/routes/modelCreation.js
@@ -9,6 +9,7 @@ var router = express.Router();
 
 var Salads = require('../../Models/Salad');
 var Request = require('../../Models/Request');
+var Response = require('../../Models/Response');
 
 //+++++++++++++++++++++++++++++++++++++
 ////Do we need to bring in other database for this???????
@@ -19,12 +20,38 @@ var Request = require('../../Models/Request');
 
 router.post('/createRequest', function(request, response) {
     Request.create({
+        for(var i = 0; i < request.event.array.length; i++){
+        event: [{
+            name: request.event.array[i].venues.venueName,
+            date: request.event.array[i].venues.events.eventDate,
+            for var j = 0; j < request.array[i].saladObject.length; j++){
+            all_salads: [{
+                amount: request.event.array[i].saladObject[j].amount,
+                salad: request.event.array[i].saladObject[j].salad
+            }]
+            }
+        }]
 
-    });
+        }
+        for(var i = 0; i < request.recipients.array.length; i++){
+            recipients: [{
+            name: request.recipients.array[i].users.orgName,
+            email: request.recipients.array[i].users.username
+        }]
+        status: false
+
+
+});
 });
 
 router.post('/createSalad', function(request, response) {
     Salads.create({
+
+    });
+});
+
+router.post('/createResponse', function(request, response) {
+    Response.create({
 
     });
 });
