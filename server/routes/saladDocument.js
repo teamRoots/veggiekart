b08d02c56d29912eafaf1752d5096163a89aa1f0
zach <bmadzac@gmail.com>
2016-02-22 13:40:26 -0600

    holder

--- server/routes/saladDocument.js
+++ server/routes/saladDocument.js
@@ -43,13 +43,13 @@ console.log('newsalad', newsalad);
             });
         }else{
             for(i = 0; i < request.body.ingredientToPush.length; i++){
-                // for(j = 0; j <ingredientPush.ingredients.length; j++){
-                //     if (request.body.ingredientToPush[i] === ingredientPush.ingredients[j]){
-                //         console.log('its a copy');
-                //     }else{
+                for(var j = 0; j < ingredientPush.ingredients.length; j++){
+                    if (request.body.ingredientToPush[i] !== ingredientPush.ingredients[j]){
+                        console.log('its a copy', i, j);
+                    }else{
             ingredientPush.ingredients.push(request.body.ingredientToPush[i]);
-            // }
-        // }
+            }
+        }
     }
 
         ingredientPush.save(function(err){
