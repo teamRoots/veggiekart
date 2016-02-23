510279f79788f5058bb6f4b8dd03e074475ba0c5
rohran01 <anthonytrohr@gmail.com>
2016-02-23 15:08:08 -0600

    allows information to be pulled from farRequest table

--- client/controllers/responseController.js
+++ client/controllers/responseController.js
@@ -5,4 +5,8 @@ app.controller('responseController', ['loginService', 'responseService', functio
   this.users = [{name: 'Scott'}, {name: 'Zach'},{name: 'Eric'},{name: 'Anthony'}];
   this.veggies = [{name: 'Parsnip', quantity: 2, unit: 'lbs.'},       {name: 'Spinach', quantity: 2, unit: 'lbs.'}, {name: 'Rutabega', quantity: 2, unit: 'lbs.'}, {name: 'Broccoli', quantity: 2, unit: 'lbs.'}];
 
+  this.test = function() {
+    console.log('user.commitment:', this.users);
+  }
+
 }]);
