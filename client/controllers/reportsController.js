4f14bee8fb654eeade083ae68d6306cf437716fc
zach <bmadzac@gmail.com>
2016-02-29 16:46:48 -0600

    peaches

--- client/controllers/reportsController.js
+++ client/controllers/reportsController.js
@@ -1,7 +1,8 @@
 app.controller('reportsController', ['createRequestService', 'eventsService', 'saladService', 'loginService', function(createRequestService, eventsService, saladService, loginService){
   this.user = loginService.user;
+  this.requests = createRequestService.data.requests;
+ console.log('alsjdflk;asjdf;lksadlfl;;lsdf', this.requests);
 
-var
 
 
 
