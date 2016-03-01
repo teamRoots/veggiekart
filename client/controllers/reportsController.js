24d02bd6d63e8ce361b2d1eaca949e71718d4716
zach <bmadzac@gmail.com>
2016-03-01 15:28:25 -0600

    possible extra for reports

--- client/controllers/reportsController.js
+++ client/controllers/reportsController.js
@@ -8,6 +8,7 @@ var gardenConfirmationDisplay = {
 this.postGardenConfirmations = function(){
   console.log('als', this.requests.length);
 var loopPush = '';
+var loopQuantityPush = '';
 var garden = {
   name: '',
   vegetables: []
@@ -31,15 +32,32 @@ for (var i = 0; i < this.requests.length; i++){
         }
         else if (this.requests[i].recipients[j].orgName != gardenConfirmationDisplay.gardens[gI].name){
           console.log('ISSUE original name', this.requests[i].recipients[j].orgName);
-          console.log('ISSUE new name', gardenConfirmationDisplay.gardens[gI].name);
+          console.log('ISSUE new name', gardenConfirmationDisplay.gardens[gI]);
 
           loopPush = true;
         }else {
           loopPush = false;
           gI = gardensLengthHolder;
-          
+          // for (var prop in this.requests[i].recipients[j].confirmations){
+          //   console.log('fourth loop', this.requests[i].recipients[j].confirmations[prop]);
+          //   console.log('fifth loop',gardenConfirmationDisplay.gardens[gI]);
+          //   for (var gJ; gJ < gardenConfirmationDisplay.gardens[gI].vegetables.length; gJ++){
+          //     console.log('fifth loop',gardenConfirmationDisplay.gardens[gI].vegetables[gJ]);
+          //
+          //     if(this.requests[i].recipients[j].confirmations[prop] != gardenConfirmationDisplay.gardens[gI].vegetables[gJ]){
+          //       loopQuantityPush = true;
+          //
+          //     }else {
+          //       loopQuantityPush = false;
+          //       gardenConfirmationDisplay.gardens[gI].vegetables[gJ].quantity += this.requests[i].recipients[j].confirmations[prop].quantity;
+          //     }
+          //   }
+          //   if(loopQuantityPush === true){
+          //     gardenConfirmationDisplay.gardens[gI].vegetables.push(this.requests[i].recipients[j].confirmations[prop]);
+          //
+          //   }
+          // }
 
-          // for (var prop in this.requests[i].recipients[j].confirmations)
         }
       }
       console.log('loopPush', loopPush);
@@ -51,8 +69,8 @@ for (var i = 0; i < this.requests.length; i++){
         garden.vegetables.push(this.requests[i].recipients[j].confirmations);
         gardenConfirmationDisplay.gardens.push(garden);
         console.log('garden', garden);
-
         j--;
+        gI--;
 
       }
 
@@ -61,35 +79,6 @@ for (var i = 0; i < this.requests.length; i++){
 this.stuff = gardenConfirmationDisplay.gardens;
 
 };
-
-
-
-//
-//   for ( i = 0; i < this.requests.length; i++){
-//     console.log('first loop hit', this.requests);
-//     for ( j = 0; j < this.requests[i].recipients.length; j++){
-//       console.log('second loop hit', this.requests[i].recipients);
-//       for (var gI = 0; gI < gardenConfirmationDisplay.gardens.length; gI++){
-//         if(this.requests[i].recipients[j]._id == gardenConfirmationDisplay.gardens[gI].gardenId){
-//
-//         }
-//       }
-//
-//       for (var prop in this.requests[i].recipients[j].confirmations) {
-//         console.log('third loop hit', this.requests[i].recipients[j].confirmations);
-//
-//           for (var k = 0; k < gardenConfirmationDisplay.gardens.length; k++){
-//                 console.log('fourth loop hit', this.requests[i].recipients[j].confirmations[prop]);
-//
-//
-//
-//
-//
-//
-//       }
-//     }
-//
-// };
 
 
 
